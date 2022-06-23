b89ee6c0875e286f4eccd497878028fba74f5c07
nimrodds <nsartral@student.42.fr>
2022-06-23 15:14:11 +0200

    minishell

---
+++ pipex/here_doc.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   here_doc.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsartral <nsartral@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/14 17:50:26 by nsartral          #+#    #+#             */
+/*   Updated: 2022/06/23 14:25:21 by nsartral         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	here_docking(t_struct *dd)
+{
+	if (pipe(dd->fd_hd_one) == -1)
+		ft_error(strerror(errno));
+	if (first_command_hd(dd) == 0)
+		return ;
+	if (last_command_hd(dd) == 0)
+		return ;
+}
+
+bool	reading_heredoc(char **argv)
+{
+	int		tmp;
+	int		ret;
+	char	buffer[255];
+	char	*limiter;
+
+	tmp = open("tmp_file", O_RDWR | O_CREAT, 0644);
+	if (tmp == -1)
+		exit(EXIT_FAILURE);
+	limiter = ft_strjoin(argv[2], "\n");
+	ret = 1;
+	while (ret)
+	{
+		write(1, "> ", 2);
+		ret = read(0, buffer, 254);
+		buffer[ret] = '\0';
+		if (strncmp(limiter, buffer, ft_strlen_bis(buffer)) == 0)
+			ret = 0;
+		if (ret)
+			write(tmp, buffer, ft_strlen_bis(buffer));
+	}
+	free(limiter);
+	return (1);
+}
+
+bool	first_command_hd(t_struct *dd)
+{
+	dd->c = 3;
+	if (parse_arguments(dd) == 0)
+		return (0);
+	if (pipe(dd->fd_hd_one) == -1)
+		return (freeing_path_and_argz(dd), 0);
+	if (reading_heredoc(dd->argv) == 0)
+		return (freeing_path_and_argz(dd), 0);
+	dd->fd_tmp = open("tmp_file", O_RDONLY, 0644);
+	if (dd->fd_tmp == -1)
+		return (freeing_path_and_argz(dd), perror("tmp_file"), 0);
+	forking(dd, dd->fd_tmp, dd->fd_hd_one[1], dd->pid_hd_one);
+	if (unlink("tmp_file") == -1)
+		ft_error(strerror(errno));
+	waitpid(dd->pid_hd_one, 0, 0);
+	freeing_path_and_argz(dd);
+	return (1);
+}
+
+bool	last_command_hd(t_struct *dd)
+{
+	dd->c = dd->argc - 2;
+	if (parse_arguments(dd) == 0)
+		return (0);
+	if (pipe(dd->fd_hd_two) == -1)
+		return (ft_printf("can't open pipe"), 0);
+	forking(dd, dd->fd_hd_one[0], dd->fd_hd_two[1], dd->pid_hd_two);
+	waitpid(dd->pid_hd_two, 0, 0);
+	freeing_path_and_argz(dd);
+	if (outfiling_hd(dd) == 0)
+		return (0);
+	return (1);
+}
+
+bool	outfiling_hd(t_struct *dd)
+{
+	if (access(dd->argv[dd->argc - 1], W_OK) == -1)
+		return (close(dd->fd[dd->c][0]),
+			ft_printf("outfile wrong permissions"), 0);
+	dd->fd_outfile = open(dd->argv[dd->argc - 1], O_CREAT | O_RDWR | O_APPEND);
+	if (dd->fd_outfile == -1)
+		return (close(dd->fd_hd_two[0]), 0);
+	dd->output_buff = (char *)malloc(sizeof(char) * 4096);
+	if (dd->output_buff == NULL)
+		return (ending_fd(dd), ft_printf("can't malloc"), 0);
+	dd->count = read(dd->fd_hd_two[0], dd->output_buff, 4096);
+	if (dd->count == -1)
+		return (free(dd->output_buff), ending_fd(dd),
+			ft_printf("can't read"), 0);
+	if (dd->count != 0)
+	{
+		dd->output_buff[dd->count] = '\0';
+		dd->count_bis = write(dd->fd_outfile, dd->output_buff, dd->count);
+		if (dd->count_bis == -1)
+			return (free(dd->output_buff), ending_fd(dd),
+				ft_printf("outfile can't write"), 0);
+	}
+	return (free(dd->output_buff), close(dd->fd_outfile),
+		close(dd->fd_hd_two[0]), 1);
+}
---
+++ pipex/pipex.h
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsartral <nsartral@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/10 02:12:35 by nsartral          #+#    #+#             */
+/*   Updated: 2022/06/15 09:56:43 by nsartral         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <stdbool.h>
+# include "./ft_printf/ft_printf.h"
+# include "./libft/libft.h"
+# include "errno.h"
+
+typedef struct s_struct
+{
+	int		argc;
+	char	**argv;
+	char	**envp;
+	char	*path;
+	char	*command;
+	char	**unix_paths;
+	char	**argz;
+	char	*buffer;
+	int		count;
+	int		count_bis;
+	char	*output_buff;
+	int		pid_one;
+	int		pid_two;
+	int		pid_hd_one;
+	int		pid_hd_two;
+	int		fd_hd_one[2];
+	int		fd_hd_two[2];
+	int		pid[4096];
+	int		fd[4096][2];
+	int		fd_tmp;
+	int		fd_infile;
+	int		fd_outfile;
+	int		c;
+	int		i;
+	int		error;
+	int		free_path;
+
+}	t_struct;
+
+char	*ft_strjoin_new(char *s1, char *s2, int flag);
+bool	get_the_path(t_struct *data);
+bool	find_path(t_struct *dd, char *unix_path);
+bool	command_trim(t_struct *data);
+bool	parse_arguments(t_struct *data);
+bool	outfiling(t_struct *data);
+void	freeing(t_struct *data);
+void	struct_init(t_struct *data, int argc, char **argv, char **envp);
+void	exiting(const char *error);
+bool	first_command(t_struct *data);
+bool	last_command(t_struct *data);
+void	forking(t_struct *dd, int fd_in, int fd_out, int pid);
+bool	is_printable(char c);
+bool	is_whitespace(char c);
+bool	is_lowercase(char c);
+void	freeing_unix(t_struct *dd);
+void	freeing_command(t_struct *dd);
+void	freeing_path(t_struct *dd);
+void	freeing_argz(t_struct *dd);
+void	freeing_path_and_argz(t_struct *dd);
+void	ending_fd(t_struct *dd);
+size_t	ft_strlen_bis(char *str);
+bool	check_arguments_bonus(int argc, char **argv, char **envp);
+bool	check_arguments(int argc, char **argv, char **envp);
+bool	check_writable(char **argv);
+bool	looping_commands(t_struct *dd);
+void	here_docking(t_struct *dd);
+bool	first_command_hd(t_struct *dd);
+bool	last_command_hd(t_struct *dd);
+bool	outfiling_hd(t_struct *dd);
+void	ft_error(char *str);
+
+#endif
