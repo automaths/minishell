62f3b329b2a864714e6ddc3793ed66c25b776eb3
nimrodds <nsartral@student.42.fr>
2022-06-25 18:36:36 +0200

    minishell

--- utils_structuring_three.c
+++ utils_structuring_three.c
@@ -2,37 +2,21 @@
 
 char	*quoting_ruling(char *str)
 {
-	char *new;
 	int i;
-	int t;
 	
 	i = -1;
-	t = 0;
 	while (str[++i])
 	{
 		if (str[i] == '"')
 		{
+			str[i] = ' ';
 			while (str[++i] && str[i] != '"')
 			{
 				if (str[i] == ' ')
 					str[i] = '*';
 			}
-			t = 1;
+			str[i] = ' ';
 		}
-	}
-	if (t == 1)
-	{
-		new = (char *)malloc(sizeof(char) * (ft_strlen(str) - 3));
-		if (new == NULL)
-			return (NULL);
-		i = -1;
-		while ((size_t)++i < (ft_strlen(str) - 3))
-		{
-			new[i] = str[i + 1];
-		}
-		new[i] = '\0';
-		free(str);
-		return (new);
 	}
 	return (str);
 }
