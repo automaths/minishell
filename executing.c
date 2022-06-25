6365fa3a3c656c81ade638d64e0c513359bf32e5
nimrodds <nsartral@student.42.fr>
2022-06-25 21:04:09 +0200

    minishell

--- executing.c
+++ executing.c
@@ -1,54 +1,97 @@
 #include "execution.h"
 
-bool	first_command(t_struct *dd)
-{
-	dd->c = 2;
-	if (parse_arguments(dd) == 0)
-		return (0);
-	if (access(dd->argv[1], F_OK) == -1)
-		return (freeing_path_and_argz(dd),
-			ft_printf("infile doesn't exist"), 0);
-	if (access(dd->argv[1], R_OK) == -1)
-		return (freeing_path_and_argz(dd),
-			ft_printf("infile wrong permissions"), 0);
-	dd->fd_infile = open(dd->argv[1], O_RDONLY);
-	if (dd->fd_infile == -1)
-		return (freeing_path_and_argz(dd),
-			ft_printf("can't open input file"), 0);
-	if (pipe(dd->fd[dd->c]) == -1)
-		return (ft_printf("can't open pipe"), 0);
-	forking(dd, dd->fd_infile, dd->fd[dd->c][1], dd->pid[dd->c]);
-	return (1);
-}
-
-bool	last_command(t_struct *dd)
-{
-	dd->c = dd->argc - 2;
-	if (parse_arguments(dd) == 0)
-		return (0);
-	if (pipe(dd->fd[dd->c]) == -1)
-		return (ft_printf("can't open pipe"), 0);
-	forking(dd, dd->fd[dd->c - 1][0], dd->fd[dd->c][1], dd->pid[dd->c]);
-	return (1);
-}
-
-bool	looping_commands(t_struct *dd)
-{
-	dd->c++;
-	while (dd->c < dd->argc - 2)
-	{
-		if (parse_arguments(dd) == 0)
-			return (0);
-		if (pipe(dd->fd[dd->c]) == -1)
-			return (freeing_path_and_argz(dd), ft_printf("can't open pipe"), 0);
-		forking(dd, dd->fd[dd->c - 1][0], dd->fd[dd->c][1], dd->pid[dd->c]);
-		freeing_path_and_argz(dd);
-		dd->c++;
-	}
-	return (1);
-}
+// void	infiling(t_command *cmd)
+// {
+// 	if (access("infile", F_OK) == -1)
+// 		return (freeing_path_and_argz(cmd),
+// 			ft_printf("infile doesn't exist"), 0);
+// 	if (access("infile", R_OK) == -1)
+// 		return (freeing_path_and_argz(cmd),
+// 			ft_printf("infile wrong permissions"), 0);
+// 	cmd->fd_in = open("infile", O_RDONLY);
+// 	if (cmd->fd_in == -1)
+// 		return (freeing_path_and_argz(cmd),
+// 			ft_printf("can't open input file"), 0);
+// }
 
 
+
+
+// bool	first_command(t_command *cmd)
+// {
+
+// 	if (pipe(cmd->fd[cmd->c]) == -1)
+// 		return (ft_printf("can't open pipe"), 0);
+// 	forking(cmd, cmd->fd_infile, cmd->fd[cmd->c][1], cmd->pid[cmd->c]);
+// 	return (1);
+// }
+
+
+
+
+// bool	last_command(t_command *cmd)
+// {
+// 	cmd->c = cmd->argc - 2;
+// 	if (parse_arguments(cmd) == 0)
+// 		return (0);
+// 	if (pipe(cmd->fd[cmd->c]) == -1)
+// 		return (ft_printf("can't open pipe"), 0);
+// 	forking(cmd, cmd->fd[cmd->c - 1][0], cmd->fd[cmd->c][1], cmd->pid[cmd->c]);
+// 	return (1);
+// }
+
+// //VARIABLE A ACTUALISER
+// void	forking(t_command *cmd, int fd_in, int fd_out, int pid)
+// {
+// 	pid = fork();
+// 	if (pid == -1)
+// 		exiting("can't fork");
+// 	if (pid == 0)
+// 	{
+// 		if (dup2(fd_in, STDIN_FILENO) == -1)
+// 			exiting(cmd->arg->argz[0]);
+// 		if (dup2(fd_out, STDOUT_FILENO) == -1)
+// 			exiting(cmd->arg->argz[0]);
+// 		close(fd_in);
+// 		close(fd_out);
+// 		if (execve(cmd->arg->path, cmd->arg->argz, cmd->arg->envp) == -1)
+// 			exiting(cmd->arg->argz[0]);
+// 	}
+// 	else
+// 	{
+// 		close(fd_in);
+// 		close(fd_out);
+// 	}
+// }
+
+// bool	looping_commands(t_command *cmd)
+// {
+// 	cmd->c++;
+// 	while (cmd->c < cmd->argc - 2)
+// 	{
+// 		if (parse_arguments(cmd) == 0)
+// 			return (0);
+// 		if (pipe(cmd->fd[cmd->c]) == -1)
+// 			return (freeing_path_and_argz(cmd), ft_printf("can't open pipe"), 0);
+// 		forking(cmd, cmd->fd[cmd->c - 1][0], cmd->fd[cmd->c][1], cmd->pid[cmd->c]);
+// 		freeing_path_and_argz(cmd);
+// 		cmd->c++;
+// 	}
+// 	return (1);
+// }
+
+
+
+// void	redirectioning(t_command *cmd)
+// {
+// 	t_token *tmp;
+	
+// 	tmp = cmd->redir;
+// 	if (tmp != NULL)
+// 	{
+// 		//FAIRE QQCH AVEC LES DUPS
+// 	}
+// }
 
 void	exec_command(t_command *cmd, char **envp)
 {
@@ -57,13 +100,15 @@ void	exec_command(t_command *cmd, char **envp)
 	if (!cmd)
 		return ;
 	tmp = cmd;
+	// infiling(cmd);
 	while (tmp != NULL)
 	{
 		parse_argument(tmp->arg, envp);
 		print_parsed(tmp);
 		print_all(tmp);
+		// redirectioning(tmp);
 		write(1, "\nLAUCHING EXEC\n", 15);
-		exec_token(tmp);
+		// exec_token(tmp);
 		write(1, "\nENDING EXEC\n", 13);
 		tmp = tmp->next;
 	}
