5d75d2a89ff7f3bc046c9eb1adbd0de668c40c12
nimrodds <nicolassartral@gmail.com>
2022-07-22 16:43:06 +0200

    minishell

--- groshell.c
+++ groshell.c
@@ -12,24 +12,41 @@
 
 #include "execution.h"
 
+void working_magic(char *str, t_env **env)
+{
+	t_command *cmd;
+	t_first *uno;
+	t_env *tmp;
+
+	uno = step_one(str);
+	if (uno->next != NULL)
+	{
+		printf("step one is success:\n");
+		// print_step_one(uno);
+		if (command_validation(uno))
+		{
+			cmd = step_two(uno, *env);
+			if (cmd != NULL)
+			{
+				// print_all(cmd);
+				tmp = exec_command(cmd);
+				if (tmp != NULL)
+					*env = tmp;
+			}
+		}
+	}
+}
 
 int	main(int argc, char **argv, char **envp)
 {
 	int			count;
 	char		*str;
-	t_command	*cmd;
-	t_first *uno;
-	t_env *env;
-	t_env *tmp;
+	t_env		*env;
 
 	(void)argc;
 	(void)argv;
 	env = env_to_list(envp);
 	str = (char *)malloc(sizeof(char) * 4096);
-	(void)envp;
-	(void)env;
-	(void)cmd;
-	(void)tmp;
 	while (1)
 	{
 		write(1, "groshell> ", 10);
@@ -37,24 +54,7 @@ int	main(int argc, char **argv, char **envp)
 		str[count] = '\0';
 		if ((ft_strncmp(str, "exit", 4) == 0) && ft_strlen(str) == 5)
 			break ;
-		uno = step_one(str);
-		if (uno != NULL)
-		{
-			printf("step one is success:\n");
-			// print_step_one(uno);
-			// str = environning(str, env);
-			if (command_validation(uno))
-			{
-				cmd = step_two(uno, env);
-				if (cmd != NULL)
-				{
-					// print_all(cmd);
-					tmp = exec_command(cmd);
-					// if (tmp != NULL)
-					// 	env = tmp;
-				}
-			}
-		}
+		working_magic(str, &env);
 	}
 	return (0);
 }
