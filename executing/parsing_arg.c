6a32dcf54e80516fecb00d604cf7818bac8917cb
nimrodds <nicolassartral@gmail.com>
2022-07-22 11:32:04 +0200

    minishell

--- executing/parsing_arg.c
+++ executing/parsing_arg.c
@@ -113,30 +113,30 @@ bool	get_the_path(t_command *cmd)
 	return (freeing_unix(cmd->arg), 1);
 }
 
-bool	parse_argument(t_command *cmd)
-{
-	char	**tmp;
-	int		i;
-	int		j;
+// bool	parse_argument(t_command *cmd)
+// {
+// 	char	**tmp;
+// 	int		i;
+// 	int		j;
 
-	token_initing(cmd);
-	if (get_the_path(cmd) == 0)
-		return (0);
-	i = 0;
-	tmp = ft_split(cmd->arg->content, ' ');
-	while (tmp[i])
-		i++;
-	if (i == 0)
-		return (0);
-	cmd->arg->argz = (char **)malloc(sizeof(char *) * (i + 2));
-	if (cmd->arg->argz == NULL)
-		return (0);
-	cmd->arg->argz[0] = cmd->arg->command;
-	j = 0;
-	while (++j <= i)
-		cmd->arg->argz[j] = tmp[j];
-	cmd->arg->argz[j] = NULL;
-	free(tmp[0]);
-	free(tmp);
-	return (1);
-}
+// 	token_initing(cmd);
+// 	if (get_the_path(cmd) == 0)
+// 		return (0);
+// 	i = 0;
+// 	tmp = ft_split(cmd->arg->content, ' ');
+// 	while (tmp[i])
+// 		i++;
+// 	if (i == 0)
+// 		return (0);
+// 	cmd->arg->argz = (char **)malloc(sizeof(char *) * (i + 2));
+// 	if (cmd->arg->argz == NULL)
+// 		return (0);
+// 	cmd->arg->argz[0] = cmd->arg->command;
+// 	j = 0;
+// 	while (++j <= i)
+// 		cmd->arg->argz[j] = tmp[j];
+// 	cmd->arg->argz[j] = NULL;
+// 	free(tmp[0]);
+// 	free(tmp);
+// 	return (1);
+// }
