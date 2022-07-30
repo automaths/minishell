0a54f2d363bf931454b2f3bd4cefd764845fb3c9
nimrodds <nsartral@student.42.fr>
2022-07-30 11:18:52 +0200

    heredoc unit test success

--- heredoc/heredoc.c
+++ heredoc/heredoc.c
@@ -63,43 +63,49 @@ char	*ft_strjoin(char *s1, char *s2)
 	return (str);
 }
 
-bool	reading_heredoc(char **argv, int fd_in)
-{
-	char	*str;
+bool	reading_heredoc(char *limiter, int fd_in);
 
-	(void)argv;
-	(void)fd_in;
-	str = (char *)malloc(sizeof(char) * 4096);
-	while (1)
-	{
-		str = readline("groshell> ");
-		if (!(ft_strncmp(str, "exit", 5)))
-			break ;
-		write(fd_in, str, ft_strlen(str));
-	}
-	exit(1);
-}
-
-int	forking_heredoc(char **argv)
+int	forking_heredoc(int fd_in, char *str)
 {
-	int fd[2];
 	int pid;
-	if (pipe(fd) == -1)
-		return (0);
-	// if (dup2(STDIN_FILENO, fd[1]) == -1)
-	// 	return (0);
-	// if (dup2(fd[1], STDOUT_FILENO) == -1)
-	// 	return (0);
-	// close (STDIN_FILENO);
-	// close(STDOUT_FILENO);
 	pid = fork();
 	if (pid == -1)
 		return (0);
 	if (pid == 0)
 	{
-		reading_heredoc(argv, fd[1]);
+		reading_heredoc(str, fd_in);
 	}
 	waitpid(pid, 0, 0);
+	return (0);
+}
+
+bool	reading_heredoc(char *limiter, int fd_in)
+{
+	char	*str;
+
+	str = (char *)malloc(sizeof(char) * 4096);
+	while (1)
+	{
+		str = readline("groshell> ");
+		if (!(ft_strncmp(str, limiter, 5)))
+			break ;
+		if (!ft_strncmp(str, "new", 4))
+		{
+			forking_heredoc(fd_in, "quit");
+		}
+		write(fd_in, str, ft_strlen(str));
+	}
+	exit(1);
+}
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	int fd[2];
+	if (pipe(fd) == -1)
+		return (0);
+	forking_heredoc(fd[1], "exit");
 	char *str;
 	int count;
 	str = (char *)malloc(sizeof(char) * 4096);
@@ -108,15 +114,6 @@ int	forking_heredoc(char **argv)
 	count = read(fd[0], str, 4096);
 	str[count] = '\0';
 	write(1, str, ft_strlen(str));
-	return (0);
-}
-
-int	main(int argc, char **argv)
-{
-	(void)argc;
-	// if (argc != 2)
-	// 	return (0);
-	forking_heredoc(argv);
 	// reading_heredoc(argv, 0);
 	return (0);
 }
