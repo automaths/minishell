93eee5f58852c90ae4a74d8d1ebf6578b13533d6
nimrodds <nsartral@student.42.fr>
2022-07-30 13:23:23 +0200

    strerror and errno unit test success

--- singleton/singleton.c
+++ singleton/singleton.c
@@ -18,6 +18,21 @@ int	singleton(int set, bool write)
 	return (singleton);
 }
 
+int	ft_strlen(char *str)
+{
+	int i = -1;
+	while (str[++i])
+		;
+	return (i);
+}
+
+void	ft_error(char *str)
+{
+	write(2, str, ft_strlen(str));
+	write(1, "\n", 1);
+	// exit(singleton(0, 0));
+}
+
 int	main(void)
 {
 	char *str;
@@ -28,9 +43,8 @@ int	main(void)
 		if (!(strncmp(str, "exit", 5)))
 			break ;
 		singleton(atoi(str), 1);
-		char c;
-		c = singleton(0, 0) + '0';
-		write(1, &c, 1);
+		ft_error(strerror(singleton(0, 0)));
 	}
 	return (0);
 }
+		// ft_error(strerror(errno));
