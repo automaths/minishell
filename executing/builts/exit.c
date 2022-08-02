b574202150711edaa1f95da4aa28f224d1aed9e8
nimrodds <nsartral@student.42.fr>
2022-08-02 15:23:13 +0200

    upgrading built-in exit

--- executing/builts/exit.c
+++ executing/builts/exit.c
@@ -6,7 +6,7 @@
 /*   By: nsartral <nsartral@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 12:07:06 by nsartral          #+#    #+#             */
-/*   Updated: 2022/08/01 17:06:57 by nsartral         ###   ########.fr       */
+/*   Updated: 2022/08/02 15:21:50 by nsartral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,28 +19,54 @@ bool	is_exit(char c)
 	return (0);
 }
 
-int	find_exit_number(char *str)
+bool	check_first(char *str)
 {
-	int	i;
+	int			i;
 
 	i = 0;
-	while (str[i] && is_whitespace(str[i]))
+	while (str[i] && ((str[i] >= 9 && str[i] <= 13) || str[i] == 32))
 		i++;
-	while (str[i] && is_exit(str[i]))
-		i++;
-	while (str[i] && is_whitespace(str[i]))
-		i++;
-	if (!str[i] || str[i] < '0' || str[i] > '9')
+	if (!str[i] || !(str[i] >= '0' &&  str[i] <= '9'))
 		return (0);
-	if (str[i])
-		return (ft_atoi(&str[i]));
-	return (0);
+	return (1);
+}
+
+void	exiting_exit(int n, t_command *cmd, bool correct)
+{
+	if (correct)
+	{
+		clean_garbage(cmd->garbage);
+		write(1, "exit\n", ft_strlen("exit\n"));
+		exit(n);
+	}
+	if (!correct)
+	{
+		write(1, "bash: exit: ", ft_strlen("bash: exit: "));
+		write(1, cmd->arg->argz[1], ft_strlen(cmd->arg->argz[1]));
+		write(1, ": numeric argument required\n", ft_strlen(": numeric argument required:\n"));
+		clean_garbage(cmd->garbage);
+		exit(2);
+	}
 }
 
 void	exec_exit(t_command *cmd)
 {
 	int	n;
 
-	n = find_exit_number(cmd->arg->argz[0]);
-	exit(n);
+	if (cmd->arg->argz[1] == NULL)
+	{
+		if (!cmd->next)
+			exiting_exit(0, cmd, 1);
+		return ;
+	}
+	if (!check_first(cmd->arg->argz[1]))
+	{
+		if (!cmd->next)
+			exiting_exit(2, cmd, 0);
+		return ;
+	}
+	n = ft_atoi(cmd->arg->argz[1]);
+	if (!cmd->next)
+		exiting_exit(n, cmd, 1);
+	return ;
 }
--- utils/libft_one.c
+++ utils/libft_one.c
@@ -6,7 +6,7 @@
 /*   By: nsartral <nsartral@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 16:56:04 by nsartral          #+#    #+#             */
-/*   Updated: 2022/08/02 00:20:13 by nsartral         ###   ########.fr       */
+/*   Updated: 2022/08/02 15:20:23 by nsartral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ int	ft_atoi(const char *str)
 
 	t = 1;
 	i = 0;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+	while (str[i] && ((str[i] >= 9 && str[i] <= 13) || str[i] == 32))
 		i++;
-	if (str[i] == '-' || str[i] == '+' || str[i] == ' ')
+	if (str[i] && (str[i] == '-' || str[i] == '+' || str[i] == ' '))
 		if (str[i++] == '-')
 			t = -1;
 	x = 0;
-	while (str[i] >= '0' && str[i] <= '9')
+	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
 	{
 		if (x * t > 2147483647)
 			return (-1);
